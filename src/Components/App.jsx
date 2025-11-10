face9c02007f5de91947ca715f664b45de722a6c
mina-y-Khalil <mina.y.khalil90@gmail.com>
2025-11-09 21:55:06 -0800

    feat: implement note creation and dynamic rendering in app

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,15 +1,31 @@
-import React from "react";
+import React, {useState} from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
 
 function App() {
+  const [notes, setNotes] = useState([]); // added a state here because this is going to change over time as we add new notes
+
+
+function addNote(newNote) {
+  setNotes(prevNotes => { // updating the notes array by adding a new note to it and keep the previous notes
+    return [...prevNotes, newNote]; // using spread operator to copy all previous notes and adding the new note at the end and this will update the state and re-render the component
+  });
+}
+
   return (
     <div>
       <Header />
-      <CreateArea />
-      <Note key={1} title="Note title" content="Note content" />
+      <CreateArea onAdd={addNote}/>
+      {notes.map((noteItem) => {
+        return <Note 
+          key={noteItem.id} 
+          title={noteItem.title} 
+          content={noteItem.content} 
+        />;
+      })}
+
       <Footer />
     </div>
   );
--- src/Components/CreateArea.jsx
+++ src/Components/CreateArea.jsx
@@ -1,12 +1,38 @@
-import React from "react";
+import React, {useState} from "react";
 
-function CreateArea() {
+function CreateArea(props) {
+  const [note, setNote] = useState({ // State to hold the note's title and content
+    title: "",
+    content: ""
+  });
+
+  function handelChange(event) {
+    const {name, value} = event.target;
+
+    setNote(prevNote => {
+      return {
+        ...prevNote,
+        [name]: value
+      };
+    } );
+  }
+
+  function submitNote(event) {
+    props.onAdd(note); // Calls the onAdd function passed via props with the current note
+    event.preventDefault(); // Prevents page reload on form submission
+    setNote({ // Resets the note state to empty strings after submission
+      title: "",
+      content: ""
+    });
+
+  }
+
   return (
     <div>
       <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
-        <button>Add</button>
+        <input value={note.title} onChange={handelChange} name="title" placeholder="Title" />
+        <textarea value={note.content} onChange={handelChange} name="content" placeholder="Take a note..." rows="3" />
+        <button onClick={submitNote}>Add</button>
       </form>
     </div>
   );
